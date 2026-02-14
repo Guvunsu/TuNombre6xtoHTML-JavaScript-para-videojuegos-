8f4a53fa9dd5c28aa0414192d0056e843377d12a
Guvunsu <gavryk_fray_rojas@hotmail.com>
2026-02-14 02:56:39 -0600

    1

--- SnakeGame/index.js
+++ SnakeGame/index.js
@@ -12,7 +12,8 @@ const snakeBorder = "black";
 const foodColor = "red";
 const foodBorder = "black";
 const unitSize = 25;
-
+//nuevo
+const playBtn = document.querySelector("#playBtn");
 // variables de logica de juego
 let runinggame = false;
 let xVelocity = unitSize;
@@ -30,19 +31,39 @@ let snake = [
 let timeOut;
 
 window.addEventListener("keydown", changeDirection);
-resetBtn.addEventListener("click", resetGame);
+window.addEventListener("click", checkAndSaveScore)//nuevo
+resetBtn.addEventListener("click", startFromButton);
+//gameStart();
+//----------------------------------------------------------------------------------------------
+function startFromButton(){
 
-gameStart();
+  if(runinggame) return; //por si acaso <-<
 
+  score = 0;
+  xVelocity = unitSize;
+  yVelocity = 0;
+
+  snake = [
+    { x: unitSize * 3, y: 0 },
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 },
+  ];
+
+  gameStart();
+}
+//------------------------------------------------------------------------------------------------
 function gameStart() {
+  playBtn.disabled = true;                       
   runinggame = true;
+  checkAndSaveScore();//nuevo
   scoreText.textContent = score;
   highScoreText.textContent = "highScore: " + highScore;
   createFood();
   drawFood();
   nextTick();
 }
-
+//--------------------------------------------------------------------------------------------
 function nextTick() {
   // update method
   if (runinggame) {
@@ -58,12 +79,12 @@ function nextTick() {
     displayGameOver();
   }
 }
-
+//--------------------------------------------------------------------------------------
 function clearBoard() {
   ctx.fillStyle = boardBackground;
   ctx.fillRect(0, 0, gameWidth, gameHeight);
 }
-
+//----------------------------------------------------------------------------------------
 function createFood() {
   function randomFood(min, max) {
     const randNum =
@@ -74,12 +95,12 @@ function createFood() {
   foodY = randomFood(0, gameHeight - unitSize);
   console.log(foodX, foodY);
 }
-
+//-------------------------------------------------------------------------------------------------------
 function drawFood() {
   ctx.fillStyle = foodColor;
   ctx.fillRect(foodX, foodY, unitSize, unitSize);
 }
-
+//--------------------------------------------------------------------------------------------------------
 function moveSnake() {
   const head = { x: snake[0].x + xVelocity, y: snake[0].y + yVelocity };
   snake.unshift(head);
@@ -92,7 +113,7 @@ function moveSnake() {
     snake.pop();
   }
 }
-
+//------------------------------------------------------------------------------------------------------
 function drawSnake() {
   ctx.fillStyle = snakeCaolor;
   ctx.strokeStyle = snakeBorder;
@@ -101,13 +122,27 @@ function drawSnake() {
     ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
   });
 }
-
-function changeDirection() {
-  const keyPressed = event.keyCode;
-  const LEFT = 37;
-  const UP = 38;
-  const RIGHT = 39;
-  const DOWN = 40;
+//------------------------------------------------------------------------------------------------------
+//modificado para que fucionara tambien las teclas WASD
+function changeDirection(event) {
+  if(!runinggame) return;//porque si no se mueve aunque este el boton de play activado
+  const keyPressed = event.key.toLowerCase();
+  const LEFT = "arrowleft";
+  const RIGHT = "arrowright";
+  const UP = "arrowup";
+  const DOWN = "arrowdown";
+  
+  const A = "a";
+  const D = "d";
+  const W = "w";
+  const S = "s";
+  //const keyPressed = event.keyPressed;
+  /*
+    const LEFT = 37;
+    const UP = 38;
+    const RIGHT = 39;
+    const DOWN = 40;
+    */
   // console.log(keyPressed);
 
   const goingUP = yVelocity == -unitSize;
@@ -116,25 +151,25 @@ function changeDirection() {
   const goingLEFT = xVelocity == -unitSize;
 
   switch (true) {
-    case keyPressed == LEFT && !goingRIGHT:
+    case (keyPressed === "arrowleft" || keyPressed === "a") && !goingRIGHT:
       xVelocity = -unitSize;
       yVelocity = 0;
       break;
-    case keyPressed == RIGHT && !goingLEFT:
+    case (keyPressed === "arrowright" || keyPressed === "d") && !goingLEFT:
       xVelocity = unitSize;
       yVelocity = 0;
       break;
-    case keyPressed == DOWN && !goingUP:
-      xVelocity = 0;
-      yVelocity = unitSize;
-      break;
-    case keyPressed == UP && !goingDOWN:
+    case (keyPressed === "arrowup" || keyPressed === "w") && !goingDOWN:
       xVelocity = 0;
       yVelocity = -unitSize;
       break;
+    case (keyPressed === "arrowdown" || keyPressed === "s") && !goingUP:
+      xVelocity = 0;
+      yVelocity = unitSize;
+      break;
   }
 }
-
+//-----------------------------------------------------------------------------------------------------
 function checkGameOver() {
   switch (true) {
     case snake[0].x < 0:
@@ -156,10 +191,11 @@ function checkGameOver() {
     }
   }
 }
-
+//------------------------------------------------------------------------------------------------
 function displayGameOver() {
-  ctx.font = "50px Arial";
-  ctx.fillStyle = "black";
+  playBtn.disabled = false;
+  ctx.font = "70px Arial";
+  ctx.fillStyle = "red";
   ctx.texAlign = "center";
   ctx.fillText("GameOver", gameWidth / 2, gameHeight / 2);
   runinggame = false;
@@ -169,7 +205,13 @@ function displayGameOver() {
     localStorage.setItem("snakeHighScore", highScore);
   }
 }
-
+//------------------------------------------------------------------------------------------
+function checkAndSaveScore() {
+  if (scoreText === null || highScore > parseInt(scoreText)) {
+    localStorage.setItem("snakeHighScore", highScore);
+  }
+}
+//-----------------------------------------------------------------------------------------
 function resetGame() {
   score = 0;
   xVelocity = unitSize;
@@ -182,6 +224,6 @@ function resetGame() {
   ];
   gameStart();
 }
+//---------------------------------------------------------------------------------------
 
-
-//nombre del jugar, meterle diseño a la pagina y boton de play
+// nombre jugador y boton de play
---
+++ SnakeGame/saveSystem.js
@@ -0,0 +1,9 @@
+
+// Hook al Game Over SIN tocar tu código
+// "Espiamos" cuando runinggame cambia a false
+
+
+
+
+// Lógica real de guardado
+
